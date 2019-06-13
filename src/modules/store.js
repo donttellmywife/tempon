2b04d606309c27a9b13de5896dae7a22401cf188
Vova Puchkov <volodymyr.puchkov@gmail.com>
2019-06-13 23:16:45 +0300

    feat: handle orders list and tabs via store

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { orders } from 'API'
+
+const { list } = orders
+const { parse, stringify } = JSON
 
 Vue.use(Vuex)
 
 
-const { parse, stringify } = JSON
 const guest = { role: 'guest' }
 const prevUser = parse(localStorage.getItem('user')) || {}
+const prevOrderStatusTab = localStorage.getItem('ordersStatusTab')
 
 
 const store = new Vuex.Store({
@@ -16,21 +20,36 @@ const store = new Vuex.Store({
       ...prevUser,
     },
 
-    // orders: {
-    //   showStatus: '',
-    // },
-    // shipments: {
-    //
-    // },
+    ui: {
+      orders: {
+        status: prevOrderStatusTab || '',
+
+        isLoading: false,
+        error: '',
+      },
+    },
+
+    orders: [],
+    shipments: {
+      fba: [],
+      fbm: [],
+    },
   },
 
 
   getters: {
     user: state => state.user,
+
+    // orders
+    orders: state => state.orders,
+    ui_ordersStatus: state => state.ui.orders.status,
+    ui_ordersLoading: state => state.ui.orders.isLoading,
+    ui_ordersError: state => state.ui.orders.error,
   },
 
 
   mutations: {
+    // AUTH
     login(state, user) {
       localStorage.setItem('user', JSON.stringify(user))
       state.user = user
@@ -38,6 +57,38 @@ const store = new Vuex.Store({
     logout(state) {
       localStorage.setItem('user', JSON.stringify(guest))
       state.user = {...guest}
+    },
+
+    // ORDERS
+    filterOrdersByStatus(state, status) {
+      localStorage.setItem('ordersStatusTab', status)
+      state.ui.orders.status = status
+    },
+    orders(state, orders) {
+      state.orders = orders
+    },
+    ordersLoading(state, isLoading) {
+      state.ui.orders.isLoading = isLoading
+    },
+    ordersError(state, error) {
+      state.ui.orders.error = error
+    },
+
+    // SHIPMENTS
+    // FBA
+  },
+
+
+  actions: {
+    loadOrders({ commit }) {
+      commit('ordersLoading', false)
+      commit('ordersLoading', '')
+
+      list()
+        .then(res => res.data)
+        .then(items => commit('orders', items))
+        .catch(err => commit('ordersError', err.toString()))
+        .then(() => commit('ordersLoading', false))
     },
   },
 })
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,11 +2,8 @@
 <main-layout>
 <h2>ORDERS</h2>
 
-<Loading v-if="isLoading" />
 
-<div v-if="!isLoading">
-  <div v-if="error">{{ error }}</div>
-
+<div>
   <ul class="nav nav-pills nav--close">
     <li class="nav-item">
       <div @click="(e) => chooseTab('')" :class="{ active: activeTab === ''}" class="nav-link">ALL</div>
@@ -23,8 +20,11 @@
   </ul>
   <br>
 
-  <ul v-if="items.length && !error">
-    <cargo-client v-for="ord in items" v-if="!activeTab || ord.status === activeTab" :key="ord._id" :cargo="ord" />
+  <Loading v-if="showLoader" />
+  <Err v-if="showError" :msg="showError" />
+
+  <ul v-if="showOrders">
+    <cargo-client v-for="ord in orders" v-if="!activeTab || ord.status === activeTab" :key="ord._id" :cargo="ord" />
   </ul>
 </div>
 </main-layout>
@@ -33,43 +33,40 @@
 
 <script>
   import { MainLayout } from 'LAYOUT'
-  import { Loading, CargoClient } from 'COMPONENT'
+  import { Loading, CargoClient, Err } from 'COMPONENT'
   import { orders } from 'API'
   const { list } = orders
+  let counter = 0
 
 
   export default {
-    data() {
-      return {
-        items: [],
+    mounted() {
+      this.$store.dispatch('loadOrders')
+    },
 
-        activeTab: this.$store.getters.user.role === 'assistant' ? 'todo' : '',
-        error: '',
-        isLoading: true,
+
+    computed: {
+      activeTab() {
+        return this.$store.getters.ui_ordersStatus
+      },
+      showError() {
+        return this.$store.getters.ui_ordersError
+      },
+      showLoader() {
+        return this.$store.getters.ui_ordersLoading
+      },
+      showOrders() {
+        return this.$store.getters.orders.length && !this.$store.getters.ui_ordersLoading && !this.$store.getters.ui_ordersError
+      },
+      orders() {
+        return this.$store.getters.orders
       }
     },
 
 
-    mounted() {
-      this.fetchData()
-    },
-
-
     methods: {
-      fetchData() {
-        this.isLoading = true
-        this.error = ''
-
-        list()
-          .then(res => res.data)
-          .then(items => this.items = items)
-          .catch(err => this.error = err.toString())
-          .then(() => this.isLoading = false)
-      },
-
-
       chooseTab(tabName) {
-        this.activeTab = tabName
+        this.$store.commit('filterOrdersByStatus', tabName)
       }
     },
 
@@ -78,6 +75,7 @@
       MainLayout,
       Loading,
       CargoClient,
+      Err,
     },
   }
 </script>
